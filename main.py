80bc8fca8efd8e492b5a97caa5465cdf90cb7881
theCrackbot <101562650+theCrackbot@users.noreply.github.com>
2022-04-04 01:42:10 +0530

    Send Motivational quotes on Mondays using Email

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+import datetime as dt
+import random
 import smtplib
 
 my_email = "freetoxplor@gmail.com"
 password = "Nsmskmdkmzk!1"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email,
-                    to_addrs="freetoxplor@yahoo.com",
-                    msg="Subject:Hello\n\nThis is a test mail.")
-connection.close()
+now = dt.datetime.now()
+day_of_week = now.weekday()
+print(day_of_week)
+if day_of_week == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs="freetoxplor@yahoo.com",
+                            msg=f"Subject:Monday Motivation\n\n{quote}")
+
+
+
